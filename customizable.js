5658f79e319c07c86bfbdbf8cda7bb6a29e9a1af
varfewo32131 <162572836+varfewo32131@users.noreply.github.com>
2024-03-23 03:40:07 +0100

    Create customizable.js

---
+++ customizable.js
@@ -0,0 +1,15 @@
+// Function to generate a random password with customizable length
+function generateRandomPassword(length) {
+    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+';
+    let password = '';
+    
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    
+    return password;
+}
+
+// Generate and display a random password with length 12
+console.log("Random Password:", generateRandomPassword(12));
