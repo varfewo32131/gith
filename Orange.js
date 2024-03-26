a7ddfdb84baae5cedfba0dfd8949957a8868511e
varfewo32131 <162572836+varfewo32131@users.noreply.github.com>
2024-03-26 06:03:05 +0100

    Create Orange.js

---
+++ Orange.js
@@ -0,0 +1,6 @@
+const fruits = ["Apple", "Banana", "Orange"];
+
+// Joining array elements into a string separated by comma
+const result = fruits.join(", ");
+
+console.log(result); // Output: "Apple, Banana, Orange"
