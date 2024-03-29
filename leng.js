8091af212ab1c4a315905d5176b5528109b19b31
varfewo32131 <162572836+varfewo32131@users.noreply.github.com>
2024-03-29 03:34:57 +0100

    Create leng.js

---
+++ leng.js
@@ -0,0 +1,11 @@
+function generateRandomPassword(length) {
+    const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+';
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    return password;
+}
+
+console.log(generateRandomPassword(12)); // Output: Random password of length 12
