a2b40e3386e4ae1f5c7b070ad0bd75a5d5e78639
varfewo32131 <162572836+varfewo32131@users.noreply.github.com>
2024-03-30 04:04:03 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+public class ConditionalExample {
+    public static void main(String[] args) {
+        int x = 10;
+        if (x > 0) {
+            System.out.println("Positive number");
+        } else if (x < 0) {
+            System.out.println("Negative number");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
