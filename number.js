088fafc4eb2fa0fec117accb221366bdb18bf35a
varfewo32131 <162572836+varfewo32131@users.noreply.github.com>
2024-04-05 04:45:08 +0200

    Create number.js

---
+++ number.js
@@ -0,0 +1,22 @@
+public class PrimeNumberCheckFunction {
+    public static void main(String[] args) {
+        int number = 29;
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
