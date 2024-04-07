9d7cb0e1d38df90cdad67fe4e3a286536723b1ab
varfewo32131 <162572836+varfewo32131@users.noreply.github.com>
2024-04-07 05:19:59 +0200

    Create numbersdgsda.js

---
+++ numbersdgsda.js
@@ -0,0 +1,7 @@
+public class SquareRoot {
+    public static void main(String[] args) {
+        double number = 16;
+        double squareRoot = Math.sqrt(number);
+        System.out.println("Square root of " + number + " is " + squareRoot);
+    }
+}
