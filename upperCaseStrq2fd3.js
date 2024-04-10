8d0b501f72fce22c22be5f96b9cbce4f832bec27
varfewo32131 <162572836+varfewo32131@users.noreply.github.com>
2024-04-10 05:16:43 +0200

    Create upperCaseStrq2fd3.js

---
+++ upperCaseStrq2fd3.js
@@ -0,0 +1,7 @@
+public class StringToUpperCaseExample {
+    public static void main(String[] args) {
+        String str = "hello, world!";
+        String upperCaseStr = str.toUpperCase();
+        System.out.println("Uppercase string: " + upperCaseStr);
+    }
+}
