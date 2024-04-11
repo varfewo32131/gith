ac53cc4d9cd51f36982da9bd2ab0fc91eeba2a70
varfewo32131 <162572836+varfewo32131@users.noreply.github.com>
2024-04-11 04:18:19 +0200

    Create bananaqewfew.js

---
+++ bananaqewfew.js
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class SortStringArray {
+    public static void main(String[] args) {
+        String[] arr = {"apple", "banana", "orange", "grape"};
+        Arrays.sort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
