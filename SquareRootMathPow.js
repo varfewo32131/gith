8669dd5e53c8914b858bdcef6c3424a0919a2cd0
varfewo32131 <162572836+varfewo32131@users.noreply.github.com>
2024-04-12 08:06:44 +0200

    Create SquareRootMathPow.js

---
+++ SquareRootMathPow.js
@@ -0,0 +1,7 @@
+public class SquareRootMathPow {
+    public static void main(String[] args) {
+        double number = 25;
+        double squareRoot = Math.pow(number, 0.5);
+        System.out.println("Square root of " + number + " is " + squareRoot);
+    }
+}
