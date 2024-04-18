654084f2f7f9ae818622df76aaf7f124bb6ad351
varfewo32131 <162572836+varfewo32131@users.noreply.github.com>
2024-04-18 05:25:24 +0200

    Create numbersdfg.js

---
+++ numbersdfg.js
@@ -0,0 +1,9 @@
+public class ArrayIteration {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println("Array elements:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
