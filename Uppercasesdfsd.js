c344198919b96f6c50847bb5c27fac4398588af7
varfewo32131 <162572836+varfewo32131@users.noreply.github.com>
2024-04-19 04:21:02 +0200

    Create Uppercasesdfsd.js

---
+++ Uppercasesdfsd.js
@@ -0,0 +1,7 @@
+public class UpperCaseConversion {
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        String upperCaseStr = str.toUpperCase();
+        System.out.println("Uppercase string: " + upperCaseStr);
+    }
+}
