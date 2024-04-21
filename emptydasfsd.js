7e446247f0aa71f6687120af506beb02052d15a2
varfewo32131 <162572836+varfewo32131@users.noreply.github.com>
2024-04-21 04:57:35 +0200

    Create emptydasfsd.js

---
+++ emptydasfsd.js
@@ -0,0 +1,10 @@
+public class StringIsEmpty {
+    public static void main(String[] args) {
+        String str = "";
+        if (str.isEmpty()) {
+            System.out.println("String is empty.");
+        } else {
+            System.out.println("String is not empty.");
+        }
+    }
+}
