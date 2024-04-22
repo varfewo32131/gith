cd9bea6f4837209acb6d6d2363258508dc5c4c0a
varfewo32131 <162572836+varfewo32131@users.noreply.github.com>
2024-04-22 05:01:37 +0200

    Create strdsfsdf.js

---
+++ strdsfsdf.js
@@ -0,0 +1,7 @@
+public class StringValueOfExample {
+    public static void main(String[] args) {
+        int number = 123;
+        String str = String.valueOf(number);
+        System.out.println("String value: " + str);
+    }
+}
