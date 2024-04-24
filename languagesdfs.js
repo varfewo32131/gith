7a2cb653b568090a8db794489efd7b362fc99f2a
varfewo32131 <162572836+varfewo32131@users.noreply.github.com>
2024-04-24 05:28:51 +0200

    Create languagesdfs.js

---
+++ languagesdfs.js
@@ -0,0 +1,7 @@
+public class LastIndexOfExample {
+    public static void main(String[] args) {
+        String str = "Java is a programming language, Java is versatile";
+        int lastIndex = str.lastIndexOf("Java");
+        System.out.println("Last index of 'Java': " + lastIndex);
+    }
+}
