28c58df409d37cd3e238f814c89d3b3083bea7b1
varfewo32131 <162572836+varfewo32131@users.noreply.github.com>
2024-05-01 04:58:51 +0200

    Create numbefsdvsdavas.js

---
+++ numbefsdvsdavas.js
@@ -0,0 +1,7 @@
+public class MaxOfTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 20;
+        int max = (num1 > num2) ? num1 : num2;
+        System.out.println("Maximum number is: " + max);
+    }
+}
