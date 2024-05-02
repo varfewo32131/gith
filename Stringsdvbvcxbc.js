540a82c26d0aca6e7e8dece78ea8f9e3bcd7a8b9
varfewo32131 <162572836+varfewo32131@users.noreply.github.com>
2024-05-02 05:36:14 +0200

    Create Stringsdvbvcxbc.js

---
+++ Stringsdvbvcxbc.js
@@ -0,0 +1,15 @@
+public class SwitchExample {
+    public static void main(String[] args) {
+        int choice = 2;
+        switch (choice) {
+            case 1:
+                System.out.println("Option 1 selected");
+                break;
+            case 2:
+                System.out.println("Option 2 selected");
+                break;
+            default:
+                System.out.println("Unknown option selected");
+        }
+    }
+}
